1316df12b88627ca94d44a2fb593b118c3bff7bf
Peter Hallam <peter@peterhallam.com>
2012-08-26 01:34:19 -0700

    Default transforms for statements and expressions.

--- src/omakase/codegeneration/JavascriptParseTreeFactory.java
+++ src/omakase/codegeneration/JavascriptParseTreeFactory.java
@@ -26,28 +26,101 @@ import java.util.List;
 /**
  */
 public final class JavascriptParseTreeFactory {
-  public static ParseTree createCall(ParseTree function, ParseTree... arguments) {
-    return new CallExpressionTree(null, function, createArguments(arguments));
+  public static ArgumentsTree createArguments(ParseTree... arguments) {
+    return createArguments(createList(arguments));
   }
 
-  private static ArgumentsTree createArguments(ParseTree... arguments) {
-    return new ArgumentsTree(null, ImmutableList.copyOf(arguments));
+  public static ArgumentsTree createArguments(ImmutableList<ParseTree> arguments) {
+    return new ArgumentsTree(null, arguments);
   }
 
-  public static ParseTree createParenExpression(ParseTree expression) {
-    return new ParenExpressionTree(null, expression);
+  public static ArrayAccessExpressionTree createArrayAccess(ParseTree object, ParseTree member) {
+    return new ArrayAccessExpressionTree(null, object,member);
   }
-  
-  public static ImmutableList<ParseTree> createList(ParseTree... arguments) {
-    return ImmutableList.copyOf(arguments);
+
+  public static ArrayLiteralExpressionTree createArrayLiteral(ImmutableList<ParseTree> elements) {
+    return new ArrayLiteralExpressionTree(null, elements);
   }
-  
-  public static ParseTree createFunction(FormalParameterListTree parameters, BlockTree body) {
-    return new FunctionExpressionTree(null, null, parameters, body);
+
+  public static ParseTree createBinaryExpression(ParseTree left, Token operator, ParseTree right) {
+    return new BinaryExpressionTree(null, left, operator, right);
   }
-  
-  public static FormalParameterListTree createFormalParameterList() {
-    return new FormalParameterListTree(null, ImmutableList.<IdentifierToken>of());
+
+  public static TokenKind transformOperator(TokenKind operator) {
+    switch (operator) {
+    case OPEN_ANGLE:
+      return TokenKind.JS_OPEN_ANGLE;
+    case CLOSE_ANGLE:
+      return TokenKind.JS_CLOSE_ANGLE;
+    case LESS_EQUAL:
+      return TokenKind.JS_LESS_EQUAL;
+    case GREATER_EQUAL:
+      return TokenKind.JS_GREATER_EQUAL;
+    case EQUAL_EQUAL:
+      return TokenKind.JS_EQUAL_EQUAL;
+    case NOT_EQUAL:
+      return TokenKind.JS_NOT_EQUAL;
+    case PLUS:
+      return TokenKind.JS_PLUS;
+    case MINUS:
+      return TokenKind.JS_MINUS;
+    case PLUS_PLUS:
+      return TokenKind.JS_PLUS_PLUS;
+    case MINUS_MINUS:
+      return TokenKind.JS_MINUS_MINUS;
+    case SHIFT_LEFT:
+      return TokenKind.JS_SHIFT_LEFT;
+    case SHIFT_RIGHT:
+      return TokenKind.JS_SHIFT_RIGHT;
+    case STAR:
+      return TokenKind.JS_STAR;
+    case PERCENT:
+      return TokenKind.JS_PERCENT;
+    case BANG:
+      return TokenKind.JS_BANG;
+    case TILDE:
+      return TokenKind.JS_TILDE;
+    case AMPERSAND:
+      return TokenKind.JS_AMPERSAND;
+    case AMPERSAND_AMPERSAND:
+      return TokenKind.JS_AMPERSAND_AMPERSAND;
+    case BAR:
+      return TokenKind.JS_BAR;
+    case BAR_BAR:
+      return TokenKind.JS_BAR_BAR;
+    case EQUAL:
+      return TokenKind.JS_EQUAL;
+    case PLUS_EQUAL:
+      return TokenKind.JS_PLUS_EQUAL;
+    case MINUS_EQUAL:
+      return TokenKind.JS_MINUS_EQUAL;
+    case STAR_EQUAL:
+      return TokenKind.JS_STAR_EQUAL;
+    case PERCENT_EQUAL:
+      return TokenKind.JS_PERCENT_EQUAL;
+    case LEFT_SHIFT_EQUAL:
+      return TokenKind.JS_LEFT_SHIFT_EQUAL;
+    case RIGHT_SHIFT_EQUAL:
+      return TokenKind.JS_RIGHT_SHIFT_EQUAL;
+    case AMPERSAND_EQUAL:
+      return TokenKind.JS_AMPERSAND_EQUAL;
+    case BAR_EQUAL:
+      return TokenKind.JS_BAR_EQUAL;
+    case HAT_EQUAL:
+      return TokenKind.JS_HAT_EQUAL;
+    case SLASH:
+      return TokenKind.JS_SLASH;
+    case SLASH_EQUAL:
+      return TokenKind.JS_SLASH_EQUAL;
+    case INSTANCEOF:
+      return TokenKind.JS_INSTANCEOF;
+    default:
+      throw new RuntimeException("Unexpected operator.");
+    }
+  }
+
+  public static Token transformOperatorToken(Token operator) {
+    return createToken(transformOperator(operator.kind));
   }
 
   public static BlockTree createBlock(ParseTree... statements) {
@@ -62,13 +135,153 @@ public final class JavascriptParseTreeFactory {
     return new BlockTree(null, ImmutableList.<ParseTree>copyOf(statements));
   }
 
+  public static BreakStatementTree createBreak() {
+    return new BreakStatementTree(null, null);
+  }
+
+  public static ParseTree createCall(ParseTree function, ParseTree... arguments) {
+    return createCall(function, createArguments(arguments));
+  }
+
+  public static ParseTree createCall(ParseTree function, ArgumentsTree args) {
+    return new CallExpressionTree(null, function, args);
+  }
+
+  public static CaseClauseTree createCaseClause(ParseTree expression, ImmutableList<ParseTree> statements) {
+    return new CaseClauseTree(null, expression, statements);
+  }
+
+  public static CatchClauseTree createCatchClause(IdentifierToken identifier, BlockTree block) {
+    return new CatchClauseTree(null, identifier, block);
+  }
+
+  public static ConditionalExpressionTree createConditional(ParseTree condition, ParseTree left, ParseTree right) {
+    return new ConditionalExpressionTree(null, condition, left, right);
+  }
+
+  public static ContinueStatementTree createContinue() {
+    return new ContinueStatementTree(null, null);
+  }
+
+  public static DebuggerStatementTree createDebugger() {
+    return new DebuggerStatementTree(null);
+  }
+
+  public static DefaultClauseTree createDefaultClause(ImmutableList<ParseTree> statements) {
+    return new DefaultClauseTree(null, statements);
+  }
+
+  public static DoStatementTree createDoStatement(ParseTree statement, ParseTree condition) {
+    return new DoStatementTree(null, statement, condition);
+  }
+
+  public static EmptyStatementTree createEmptyStatement() {
+    return new EmptyStatementTree(null);
+  }
+
+  public static ExpressionStatementTree createExpressionStatement(ParseTree expression) {
+    return new ExpressionStatementTree(null, expression);
+  }
+
+  public static ForInStatementTree createForInStatement(ParseTree element, ParseTree collection, ParseTree body) {
+    return new ForInStatementTree(null, element, collection, body);
+  }
+
+  public static ForStatementTree createForStatement(ParseTree initializer, ParseTree condition, ParseTree increment, ParseTree body) {
+    return new ForStatementTree(null, initializer, condition, increment, body);
+  }
+
+  public static FormalParameterListTree createFormalParameterList() {
+    return new FormalParameterListTree(null, ImmutableList.<IdentifierToken>of());
+  }
+
+  public static FormalParameterListTree createFormalParameterList(ImmutableList<IdentifierToken> parameters) {
+    return new FormalParameterListTree(null, parameters);
+  }
+
+  public static IdentifierExpressionTree createIdentifier(String value) {
+    return createIdentifier(createIdentifierToken(value));
+  }
+
+  public static IdentifierExpressionTree createIdentifier(omakase.syntax.tokens.IdentifierToken identifier) {
+    return createIdentifier(identifier.value);
+  }
+
+  public static IdentifierExpressionTree createIdentifier(IdentifierToken identifier) {
+    return new IdentifierExpressionTree(null, identifier);
+  }
+
+  public static IfStatementTree createIfStatement(ParseTree condition, ParseTree ifClause, ParseTree elseClause) {
+    return new IfStatementTree(null, condition, ifClause, elseClause);
+  }
+
+  public static LiteralExpressionTree createLiteral(Token literal) {
+    return new LiteralExpressionTree(null, literal);
+  }
+
+  public static NewExpressionTree createNew(ParseTree constructor, ArgumentsTree arguments) {
+    return new NewExpressionTree(null, constructor, arguments);
+  }
+
+  public static ParseTree createParenExpression(ParseTree expression) {
+    return new ParenExpressionTree(null, expression);
+  }
+
+  public static ParseTree createPostfixExpression(ParseTree operand, Token operator) {
+    return new PostfixExpressionTree(null, operand, operator);
+  }
+
+  public static ReturnStatementTree createReturnStatement(ParseTree value) {
+    return new ReturnStatementTree(null, value);
+  }
+
+  public static SwitchStatementTree createSwitchStatement(ParseTree expression, ImmutableList<ParseTree> caseClauses) {
+    return new SwitchStatementTree(null, expression, caseClauses);
+  }
+
+  public static ThisExpressionTree createThis() {
+    return new ThisExpressionTree(null);
+  }
+
+  public static ThrowStatementTree createThrowStatement(ParseTree expression) {
+    return new ThrowStatementTree(null, expression);
+  }
+
+  public static TryStatementTree createTryStatement(BlockTree body, CatchClauseTree catchClause, BlockTree finallyClause) {
+    return new TryStatementTree(null, body, catchClause, finallyClause);
+  }
+
+  public static UnaryExpressionTree createUnaryExpression(Token operator, ParseTree operand) {
+    return new UnaryExpressionTree(null, operator, operand);
+  }
+
+  public static VariableDeclarationTree createVariableDeclaration(String name, ParseTree initializer) {
+    return new VariableDeclarationTree(null, createIdentifierToken(name), initializer);
+  }
+
+  public static VariableStatementTree createVariableStatement(ImmutableList<ParseTree> declarations) {
+    return new VariableStatementTree(null, declarations);
+  }
+
+  public static WhileStatementTree createWhileStatement(ParseTree condition, ParseTree body) {
+    return new WhileStatementTree(null, condition, body);
+  }
+
+  public static ImmutableList<ParseTree> createList(ParseTree... arguments) {
+    return ImmutableList.copyOf(arguments);
+  }
+  
+  public static ParseTree createFunction(FormalParameterListTree parameters, BlockTree body) {
+    return new FunctionExpressionTree(null, null, parameters, body);
+  }
+  
   public static ParseTree createDottedName(String... names) {
     ParseTree result = null;
     for (String name: names) {
       if (result == null) {
         result = createIdentifier(name);
       } else {
-        result = new MemberExpressionTree(null, result, new omakase.syntax.tokens.javascript.IdentifierToken(null, name));
+        result = new MemberExpressionTree(null, result, createIdentifierToken(name));
       }
     }
     return result;
@@ -76,31 +289,15 @@ public final class JavascriptParseTreeFactory {
 
   public static ParseTree createAssignmentStatement(ParseTree left, ParseTree right) {
     return createExpressionStatement(
-        createBinaryExpression(left, TokenKind.JS_EQUAL, right)
+        createBinaryExpression(left, createToken(TokenKind.JS_EQUAL), right)
     );
   }
 
-  private static ParseTree createIdentifier(String value) {
-    return createIdentifier(new IdentifierToken(null, value));
-  }
-
-  public static ParseTree createIdentifier(omakase.syntax.tokens.IdentifierToken identifier) {
-    return createIdentifier(identifier.value);
-  }
-
-  public static ParseTree createIdentifier(IdentifierToken identifier) {
-    return new IdentifierExpressionTree(null, identifier);
-  }
-
-  public static ParseTree createBinaryExpression(ParseTree left, TokenKind kind, ParseTree right) {
-    return new BinaryExpressionTree(null, left, createToken(kind), right);
+  public static IdentifierToken createIdentifierToken(String value) {
+    return new IdentifierToken(null, value);
   }
 
   public static Token createToken(TokenKind kind) {
     return new Token(kind, null);
-  }
-
-  public static ParseTree createExpressionStatement(ParseTree expression) {
-    return new ExpressionStatementTree(null, expression);
   }
 }
--- src/omakase/codegeneration/JavascriptTransformer.java
+++ src/omakase/codegeneration/JavascriptTransformer.java
@@ -14,9 +14,50 @@
 
 package omakase.codegeneration;
 
+import com.google.common.collect.ImmutableList;
 import omakase.syntax.ParseTreeTransformer;
+import omakase.syntax.tokens.Token;
+import omakase.syntax.tokens.TokenKind;
+import omakase.syntax.tokens.javascript.IdentifierToken;
+import omakase.syntax.tokens.javascript.NumericLiteralToken;
+import omakase.syntax.tokens.javascript.StringLiteralToken;
 import omakase.syntax.trees.*;
-import omakase.syntax.trees.javascript.ProgramTree;
+import omakase.syntax.trees.ArgumentsTree;
+import omakase.syntax.trees.ArrayAccessExpressionTree;
+import omakase.syntax.trees.ArrayLiteralExpressionTree;
+import omakase.syntax.trees.BinaryExpressionTree;
+import omakase.syntax.trees.BlockTree;
+import omakase.syntax.trees.BreakStatementTree;
+import omakase.syntax.trees.CallExpressionTree;
+import omakase.syntax.trees.CaseClauseTree;
+import omakase.syntax.trees.CatchClauseTree;
+import omakase.syntax.trees.ConditionalExpressionTree;
+import omakase.syntax.trees.ContinueStatementTree;
+import omakase.syntax.trees.DebuggerStatementTree;
+import omakase.syntax.trees.DefaultClauseTree;
+import omakase.syntax.trees.DoStatementTree;
+import omakase.syntax.trees.EmptyStatementTree;
+import omakase.syntax.trees.ExpressionStatementTree;
+import omakase.syntax.trees.ForInStatementTree;
+import omakase.syntax.trees.ForStatementTree;
+import omakase.syntax.trees.FormalParameterListTree;
+import omakase.syntax.trees.FunctionExpressionTree;
+import omakase.syntax.trees.IdentifierExpressionTree;
+import omakase.syntax.trees.IfStatementTree;
+import omakase.syntax.trees.LiteralExpressionTree;
+import omakase.syntax.trees.NewExpressionTree;
+import omakase.syntax.trees.ParenExpressionTree;
+import omakase.syntax.trees.PostfixExpressionTree;
+import omakase.syntax.trees.ReturnStatementTree;
+import omakase.syntax.trees.SwitchStatementTree;
+import omakase.syntax.trees.ThisExpressionTree;
+import omakase.syntax.trees.ThrowStatementTree;
+import omakase.syntax.trees.TryStatementTree;
+import omakase.syntax.trees.UnaryExpressionTree;
+import omakase.syntax.trees.VariableDeclarationTree;
+import omakase.syntax.trees.VariableStatementTree;
+import omakase.syntax.trees.WhileStatementTree;
+import omakase.syntax.trees.javascript.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -29,12 +70,231 @@ import static omakase.codegeneration.JavascriptParseTreeFactory.*;
 public class JavascriptTransformer extends ParseTreeTransformer {
   @Override
   protected ParseTree transform(SourceFileTree tree) {
-    return new ProgramTree(null, transformList(tree.declarations));
+    return new omakase.syntax.trees.javascript.ProgramTree(null, transformList(tree.declarations));
+  }
+
+  @Override
+  protected omakase.syntax.trees.javascript.ArgumentsTree transform(ArgumentsTree tree) {
+    return createArguments(transformList(tree.arguments));
+  }
+
+  @Override
+  protected ParseTree transform(ArrayAccessExpressionTree tree) {
+    return createArrayAccess(transformAny(tree.object), transformAny(tree.member));
+  }
+
+  @Override
+  protected ParseTree transform(ArrayLiteralExpressionTree tree) {
+    return createArrayLiteral(transformList(tree.elements));
+  }
+
+  @Override
+  protected ParseTree transform(BinaryExpressionTree tree) {
+    return createBinaryExpression(transformAny(tree.left), transformOperatorToken(tree.operator), transformAny(tree.right));
+  }
+
+  @Override
+  protected omakase.syntax.trees.javascript.BlockTree transform(BlockTree tree) {
+    return createBlock(transformList(tree.statements));
+  }
+
+  @Override
+  protected ParseTree transform(BreakStatementTree tree) {
+    return createBreak();
+  }
+
+  @Override
+  protected ParseTree transform(CallExpressionTree tree) {
+    return createCall(transformAny(tree.function), transform(tree.arguments).asJavascriptArguments());
+  }
+
+  @Override
+  protected ParseTree transform(CaseClauseTree tree) {
+    return createCaseClause(transformAny(tree.expression), transformList(tree.statements));
+  }
+
+  @Override
+  protected omakase.syntax.trees.javascript.CatchClauseTree transform(CatchClauseTree tree) {
+    return createCatchClause(createIdentifierToken(tree.identifier.value), transformAny(tree.block).asJavascriptBlock());
+  }
+
+  @Override
+  protected ParseTree transform(ConditionalExpressionTree tree) {
+    return createConditional(transformAny(tree.condition), transformAny(tree.left), transformAny(tree.right));
+  }
+
+  @Override
+  protected ParseTree transform(ContinueStatementTree tree) {
+    return createContinue();
+  }
+
+  @Override
+  protected ParseTree transform(DebuggerStatementTree tree) {
+    return createDebugger();
+  }
+
+  @Override
+  protected ParseTree transform(DefaultClauseTree tree) {
+    return createDefaultClause(transformList(tree.statements));
+  }
+
+  @Override
+  protected ParseTree transform(DoStatementTree tree) {
+    return createDoStatement(transformAny(tree.statement), transformAny(tree.condition));
+  }
+
+  @Override
+  protected ParseTree transform(EmptyStatementTree tree) {
+    return createEmptyStatement();
+  }
+
+  @Override
+  protected ParseTree transform(ExpressionStatementTree tree) {
+    return createExpressionStatement(transformAny(tree.expression));
+  }
+
+  @Override
+  protected ParseTree transform(ForInStatementTree tree) {
+    return createForInStatement(transformAny(tree.element), transformAny(tree.collection), transformAny(tree.body));
+  }
+
+  @Override
+  protected ParseTree transform(ForStatementTree tree) {
+    return createForStatement(transformAny(tree.initializer), transformAny(tree.condition), transformAny(tree.increment), transformAny(tree.body));
+  }
+
+  @Override
+  protected ParseTree transform(FormalParameterListTree tree) {
+    return createFormalParameterList(transformFormalParameterList(tree.parameters));
+  }
+
+  private ImmutableList<omakase.syntax.tokens.javascript.IdentifierToken> transformFormalParameterList(ImmutableList<ParseTree> parameters) {
+    ImmutableList.Builder<IdentifierToken> names = new ImmutableList.Builder<IdentifierToken>();
+    for (ParseTree parameter : parameters) {
+      names.add(createIdentifierToken(parameter.asParameterDeclaration().name.value));
+    }
+    return names.build();
+  }
+
+  @Override
+  protected ParseTree transform(FunctionExpressionTree tree) {
+    // TODO:
+    return super.transform(tree);
+  }
+
+  @Override
+  protected ParseTree transform(IdentifierExpressionTree tree) {
+    return createIdentifier(tree.name.value);
+  }
+
+  @Override
+  protected ParseTree transform(IfStatementTree tree) {
+    return createIfStatement(transformAny(tree.condition), transformAny(tree.ifClause), transformAny(tree.elseClause));
+  }
+
+  @Override
+  protected ParseTree transform(LiteralExpressionTree tree) {
+    return createLiteral(transformLiteral(tree.value));
+  }
+
+  private Token transformLiteral(Token value) {
+    switch (value.kind) {
+    case FALSE:
+      return new Token(TokenKind.JS_FALSE, null);
+    case NUMBER:
+      return new NumericLiteralToken(null, value.asNumericLiteral().value);
+    case STRING:
+      return new StringLiteralToken(null, value.asStringLiteral().value);
+    case NULL:
+      return new Token(TokenKind.JS_NULL, null);
+    case TRUE:
+      return new Token(TokenKind.JS_TRUE, null);
+    default:
+      throw new RuntimeException("unexpected literal kind");
+    }
+  }
+
+  @Override
+  protected ParseTree transform(NewExpressionTree tree) {
+    return createNew(transformAny(tree.constructor), transform(tree.arguments));
+  }
+
+  @Override
+  protected ParseTree transform(ParameterDeclarationTree tree) {
+    throw new RuntimeException("params should get transformed by formals");
+  }
+
+  @Override
+  protected ParseTree transform(ParenExpressionTree tree) {
+    return createParenExpression(transformAny(tree.expression));
+  }
+
+  @Override
+  protected ParseTree transform(PostfixExpressionTree tree) {
+    return createPostfixExpression(transformAny(tree.operand), transformOperatorToken(tree.operator));
+  }
+
+  @Override
+  protected ParseTree transform(ReturnStatementTree tree) {
+    return createReturnStatement(transformAny(tree.value));
+  }
+
+  @Override
+  protected ParseTree transform(SwitchStatementTree tree) {
+    return createSwitchStatement(transformAny(tree.expression), transformList(tree.caseClauses));
+  }
+
+  @Override
+  protected ParseTree transform(ThisExpressionTree tree) {
+    return createThis();
+  }
+
+  @Override
+  protected ParseTree transform(ThrowStatementTree tree) {
+    return createThrowStatement(transformAny(tree.expression));
+  }
+
+  @Override
+  protected ParseTree transform(TryStatementTree tree) {
+    omakase.syntax.trees.javascript.CatchClauseTree catchClause = null;
+    if (tree.catchClause != null) {
+      catchClause = transform(tree.catchClause);
+    }
+    omakase.syntax.trees.javascript.BlockTree finallyClause = null;
+    if (tree.finallyClause != null) {
+      finallyClause = transform(tree.finallyClause);
+    }
+    return createTryStatement(transform(tree.body), catchClause, finallyClause);
+  }
+
+  @Override
+  protected ParseTree transform(UnaryExpressionTree tree) {
+    return createUnaryExpression(transformOperatorToken(tree.operator), transformAny(tree.operand));
+  }
+
+  @Override
+  protected ParseTree transform(VariableDeclarationTree tree) {
+    return createVariableDeclaration(tree.name.value, transformAny(tree.initializer));
+  }
+
+  @Override
+  protected ParseTree transform(VariableStatementTree tree) {
+    return createVariableStatement(transformList(tree.declarations));
+  }
+
+  @Override
+  protected ParseTree transform(WhileStatementTree tree) {
+    return createWhileStatement(transformAny(tree.condition), transformAny(tree.body));
+  }
+
+  @Override
+  protected ParseTree transform(MethodDeclarationTree tree) {
+    throw new RuntimeException("Methods should be handled by class transformer");
   }
 
   @Override
   protected ParseTree transform(ClassDeclarationTree tree) {
-    return new ClassTransformer().transform(tree);
+    return ClassTransformer.transformClass(tree);
   }
 
   private static class ClassTransformer {
@@ -74,6 +334,10 @@ public class JavascriptTransformer extends ParseTreeTransformer {
       return createAssignmentStatement(
           createDottedName(className, "prototype", method.name.value),
           createFunction(createFormalParameterList(), createBlock()));
+    }
+
+    public static ParseTree transformClass(ClassDeclarationTree tree) {
+      return new ClassTransformer().transform(tree);
     }
   }
 }
